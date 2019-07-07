b8ade46bff64b8436c5afd126d2ded1bde89e7ef
dngrey <qwerty.qq185@gmail.com>
2019-07-07 19:02:10 +0300

    refresh data

--- src/Containers/Home/index.js
+++ src/Containers/Home/index.js
@@ -1,31 +1,73 @@
 import React, { PureComponent } from 'react';
-import { SafeAreaView, ScrollView, View, Text, ActivityIndicator } from 'react-native';
+import {
+  SafeAreaView,
+  ScrollView,
+  View,
+  ActivityIndicator,
+  RefreshControl
+} from "react-native";
 import { connect } from 'react-redux';
 import { ListItem } from 'react-native-elements'
 import { getSomeData, isFetching} from 'AppRedux';
 
 class Home extends PureComponent {
 
+  state = {
+    refreshing: false
+  };
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = () => {
+    const { isFetching, getSomeData } = this.props;
+    isFetching();
+    getSomeData();
+  };
+
+  refreshData = () => {
+    const { getSomeData } = this.props;
+    this.setState({ refreshing: true });
+    Promise.resolve(getSomeData()).then(() => {
+      setTimeout(() => {
+        this.setState({ refreshing: false });
+      }, 600);
+    });
+  };
+
   render() {
-    const { someData, isLoading } = this.props;
+    const { someData, isLoading, message } = this.props;
 
     return (
       <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <ScrollView 
+          contentInsetAdjustmentBehavior="automatic"
+          refreshControl={
+            <RefreshControl
+              refreshing={this.state.refreshing}
+              onRefresh={this.refreshData}
+            />
+          }
+        >
           {!!isLoading
-            ? <ActivityIndicator /> 
-            : <View>
-              {someData && someData.articles.map((elem, idx) => (
+            ? <ActivityIndicator size={'large'}/>
+            : someData
+            ? <View>
+              {someData.articles.map((elem, idx) => (
                   <ListItem
                     key={idx}
                     leftAvatar={{ source: { uri: elem.urlToImage } }}
                     title={elem.title}
                     subtitle={elem.description}
                     bottomDivider
-                    onPress={() => alert(1)}
+                    onPress={() => {}}
                   />
                 ))}
-            </View>
+              </View>
+            : message
+            ? <Text>{`Something went wrong!\n${message}`}</Text>
+            : null
           }
         </ScrollView>
       </SafeAreaView>
@@ -36,14 +78,15 @@ class Home extends PureComponent {
 const mapStateToProps = state => {
   return {
     someData: state.home.someData,
-    isLoading: state.home.isLoading
+    isLoading: state.home.isLoading,
+    message: state.home.message
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    isFetching: dispatch(isFetching()),
-    getSomeData: dispatch(getSomeData())
+    isFetching: () => dispatch(isFetching()),
+    getSomeData: () => dispatch(getSomeData())
   };
 };
 
